8e1e995105580f3bee45f4fbca821825e300ff7b
kimbohn <810478206@qq.com>
2020-09-21 16:39:36 +0800

    refactor: request

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,10 @@
 import { Toast } from 'antd-mobile';
+import { history } from 'umi';
 import { addScriptToHead, setRemIfResize } from './utils/customUtils';
 import { isIOSEnv } from '@/utils/env';
+import { customOptions, requestInstance } from './utils/request';
+import { ResponseTplt } from './services/type';
+import { isResponseOk } from './utils/model';
 const defaultSettings = require('./defaultSettings');
 
 const { iconfontUrl } = defaultSettings;
@@ -15,9 +19,13 @@ setRemIfResize(750, 75);
 if (!isIOSEnv()) {
   addScriptToHead('https://as.alipayobjects.com/g/component/fastclick/1.0.6/fastclick.js', () => {
     if ('addEventListener' in document) {
-      document.addEventListener('DOMContentLoaded', function () {
-        window.FastClick.attach(document.body);
-      }, false);
+      document.addEventListener(
+        'DOMContentLoaded',
+        function () {
+          window.FastClick.attach(document.body);
+        },
+        false,
+      );
     }
   });
 }
@@ -25,3 +33,32 @@ if (!isIOSEnv()) {
 Toast.config({
   mask: false,
 });
+
+requestInstance.interceptors.response.use(async (response, options) => {
+  const {
+    headers: { token },
+  } = options as any;
+  if (response.ok) {
+    const copyResponse = response.clone();
+    const _response: ResponseTplt = await copyResponse.json();
+    // 判断数据结构是否为本系统后台响应
+    if (_response.msg && token && !isResponseOk(_response)) {
+      if (_response.code === 4000) {
+        // token 失效
+        Toast.fail('登录失效', 1500, () => {
+          history.replace('/user/login');
+          localStorage.clear();
+        });
+      } else if (_response.code === 3014) {
+        Toast.fail('暂无权限');
+      }
+    }
+  }
+  return response;
+});
+
+customOptions(() => ({
+  headers: {
+    token: `${localStorage.getItem('token')}`,
+  },
+}));
