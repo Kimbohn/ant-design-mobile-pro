5d5fa9beedbb1c88b491e10ffb7a0f9cc7710b0a
kimbohn <810478206@qq.com>
2020-09-21 16:34:56 +0800

    refactor: User

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Toast, Button } from 'antd-mobile';
-import { connect } from 'dva';
+import { history } from 'umi';
 import { createForm } from 'rc-form';
 import Paper from '@/components/Paper';
 import Form from '@/components/Form';
@@ -8,6 +8,8 @@ import { ItemConfig } from '@/components/Form/Props';
 import CustomIcon from '@/components/CustomIcon';
 import { Dispatch, ConnectState } from '@/models/connect';
 import styles from './Login.less';
+import { useLogin } from '@/hooks/user';
+import { setAuthority } from '@/utils/authority';
 const defaultSettings = require('@/defaultSettings');
 
 const { publicPath } = defaultSettings;
@@ -20,110 +22,101 @@ const setItems: (form: any, config: any) => ItemConfig[] = (form, config) => {
   return [
     {
       field: 'username',
-      label: <CustomIcon type="user" style={iconStyle} />,
+      label: <CustomIcon type='user' style={iconStyle} />,
       fieldProps: {
         rules: [
           {
-            required: true, message: '请输入姓名',
-          }
+            required: true,
+            message: '请输入姓名',
+          },
         ],
       },
       componentProps: {
-        placeholder: "请输入帐号：admin",
+        placeholder: '请输入帐号：admin',
         autoComplete: 'off',
         labelNumber: 1,
       },
     },
     {
       field: 'password',
-      label: <CustomIcon type="password" style={iconStyle} />,
+      label: <CustomIcon type='password' style={iconStyle} />,
       fieldProps: {
         rules: [
           {
-            required: true, message: '请输入密码',
-          }
+            required: true,
+            message: '请输入密码',
+          },
         ],
       },
       componentProps: {
-        type: "password",
-        placeholder: "请输入密码：123456",
+        type: 'password',
+        placeholder: '请输入密码：123456',
         autoComplete: 'off',
         labelNumber: 1,
-        onKeyPress: handleKeyPress
+        onKeyPress: handleKeyPress,
       },
     },
   ];
-}
+};
 
 export interface LoginProps {
   form: any;
-  dispatch: Dispatch;
-  login: any;
-  loading: boolean;
 }
 
-class Login extends React.PureComponent<LoginProps> {
-  handleSubmit = () => {
-    const { form, dispatch } = this.props;
-    form.validateFields((err, values) => {
+export default createForm()((props: LoginProps) => {
+  const { form } = props;
+  const { run: runLogin, loading } = useLogin();
+
+  const handleSubmit = () => {
+    form.validateFields(async (err, values) => {
       console.log(err, values);
       if (err) {
         return;
-      };
-      dispatch({
-        type: 'login/login',
-        payload: {
-          ...values,
-        },
-      });
-    })
-  }
+      }
 
-  handleKeyPress = (event) => {
+      const response = await runLogin(values);
+
+      setAuthority(response.currentAuthority);
+      if (response.status === 'ok') {
+        history.replace('/');
+      } else {
+        Toast.fail('帐号或密码错误！');
+      }
+    });
+  };
+
+  const handleKeyPress = (event) => {
     if (event.key === 'Enter') {
-      this.handleSubmit();
+      handleSubmit();
     }
-  }
+  };
 
-  render() {
-    const icon = (
-      <div className={styles.logoWrapper}>
-        <img
-          src={logo}
-          alt=""
+  const icon = (
+    <div className={styles.logoWrapper}>
+      <img src={logo} alt='' />
+    </div>
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <Paper icon={icon}>
+        <Form
+          style={{ marginTop: '.64rem' }}
+          form={form}
+          items={setItems(form, { handleKeyPress })}
+          buttonText={null}
+          errorsFooter={false}
+          hideRequiredMark
         />
-      </div>
-    );
-    const { form, loading } = this.props;
-
-    return (
-      <div className={styles.wrapper}>
-        <Paper icon={icon}>
-          <Form
-            style={{ marginTop: '.64rem' }}
-            form={form}
-            items={setItems(form, { handleKeyPress: this.handleKeyPress })}
-            buttonText={null}
-            errorsFooter={false}
-            hideRequiredMark
-          />
-          <Button
-            type='primary'
-            style={{ margin: '.64rem 0.08rem' }}
-            loading={loading}
-            onClick={this.handleSubmit}
-          >
-            登录
-          </Button>
-        </Paper>
-      </div>
-    );
-  }
-}
-
-export default connect(
-  ({ login, loading }: ConnectState) => ({
-    login,
-    loading: loading.effects['login/login'],
-  })
-)(createForm()(Login));
+        <Button
+          type='primary'
+          style={{ margin: '.64rem 0.08rem' }}
+          loading={loading}
+          onClick={handleSubmit}
+        >
+          登录
+        </Button>
+      </Paper>
+    </div>
+  );
+});
